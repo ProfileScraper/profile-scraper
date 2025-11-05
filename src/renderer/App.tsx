90922e047e4a05116d8f1b28e0af2426d5d49c3f
Joel Rhine <joel@pie.org>
2025-11-05 15:58:52 -0600

    feat: add main App component and HTML template

---
+++ src/renderer/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Dashboard } from './components/Dashboard';
+import './styles/index.css';
+
+export function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <Dashboard />
+    </div>
+  );
+}
